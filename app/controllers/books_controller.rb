716e2c60a66039392e20dbda278e94682932839e
pokori20 <93557699+pokori20@users.noreply.github.com>
2021-12-16 11:57:15 +0000

    top画面にsign upとLog inリンクの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,38 @@
 class BooksController < ApplicationController
+  def new
+    @book = book.new
+  end
+
+  def create
+    @book = PostImage.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+    @books = Book.all
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @book = PostImage.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
