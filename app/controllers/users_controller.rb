b70829cdbce86099f4eaafd38dec00bc8e154fe5
pokori20 <93557699+pokori20@users.noreply.github.com>
2021-12-18 05:04:16 +0000

    topページの編集とマイページへの遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page]).reverse_order
+    @books = @user.books
   end
 
   def edit
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   def index
     @users = User.all
-    
+  end
 
     private
 
