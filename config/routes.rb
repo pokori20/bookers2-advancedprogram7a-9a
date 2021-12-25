1b91cb1641306307c7d316388d2286c85c33b5bf
pokori20 <93557699+pokori20@users.noreply.github.com>
2021-12-25 11:17:49 +0000

    [finish]応用課題3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :index]
 
