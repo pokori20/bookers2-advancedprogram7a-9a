024e28f4f9df2cfcb44cfdb67a374a4f7d4eab65
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-04 03:13:55 +0000

    destroyいいねの非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,11 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -13,7 +14,7 @@ class BookCommentsController < ApplicationController
 
     book_comment.destroy
 
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   private
