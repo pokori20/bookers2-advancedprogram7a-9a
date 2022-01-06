b649daa84aee0b5871db70af977f1e5b54f7cf0b
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-06 07:38:21 +0000

    :sparkles: [finish]応用課題6
    コメント投稿機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,22 @@
 class BookCommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    # @book = Book.find(params[:book_id])
-    # @comment = current_user.book_comments.new(book_comment_params)
-    # @comment.book_id = @book.id
-    # @comment.save
+    @book_comment= BookComment.new
+
     # redirect_back(fallback_location: root_path)
 
   end
 
   def destroy
-    @book_comment = BookComment.find(params[:id])
-
+    book_comment = BookComment.find(params[:id])
     book_comment.destroy
-    # book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    # @book_comment = BookComment.find(params[:id])
-    # @book = Book.find(params[:book_id])
-    # @book_comment.destroy
+
+    @book = Book.find(params[:book_id])
+    @book_comment= BookComment.new
 
     # redirect_back(fallback_location: root_path)
   end
