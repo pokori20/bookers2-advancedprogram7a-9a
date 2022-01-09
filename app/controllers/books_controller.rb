fa9cc89bd6f11b31deccbf8e8101898aff0354e0
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-01-09 11:30:35 +0000

    :memo: 応用課題7a
    新たに使用したメソッドの使い方の説明sort includes 時間の範囲指定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,10 +19,18 @@ class BooksController < ApplicationController
     @book = Book.new
     @user = current_user
     
+    # Time.current.at現在の時刻を取得つまり to = 今日の終わり
     to = Time.current.at_end_of_day
+    # 6日前のはじめ
     from  = (to - 6.day).at_beginning_of_day
+    # sizeでもcountでも表示は問題ないが、反応スピードの関係でsizeを使用
+    # ...は範囲演算子例　1..5 は1～5 1...5は終端を含まないので1～4の意味
+    # Book.allと書かない理由はincludes(引数)と記述した方が余分なsqlを発行しなくなるため軽くなる
+    # Arrayオブジェクト.sort {|a, b| ... } Arrayは配列の意味　配列の中身をa bを用いてどのように比較するか可能
         @books = Book.includes(:favorited_users).
           sort {|a,b| 
+          # a <=> b基本的な比較演算子　 a < b であれば -1、a == b であれば 0、a > b であれば 1、比較できない時は nil を返えす。
+          # bを先に記述して降順にしている。
         b.favorited_users.includes(:favorites).where(created_at: from...to).size <=> 
         a.favorited_users.includes(:favorites).where(created_at: from...to).size
       }
